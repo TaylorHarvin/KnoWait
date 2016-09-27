5793537a21d7ae60a1b8d26a9a2bdd2a64922827
Sina Serati <WinterSina@Sinas-MacBook-Pro.local>
2016-09-27 08:00:59 -0500

    adding android version to git branch

---
+++ AndroidKnoWait/app/src/main/java/com/hapticgate/androidknowait/KnowWaitMain.java
@@ -0,0 +1,13 @@
+package com.hapticgate.androidknowait;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KnowWaitMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_know_wait_main);
+    }
+}
