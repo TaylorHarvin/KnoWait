02a1721a3c32384734fd44cb67d7b1d734ef177f
Preston <preston.james.nalls@gmail.com>
2016-10-02 16:47:10 -0500

    Added placeholder map for Android version of the app

--- App/testing/python_php.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$myArray = array(5,6,7,8,9);
-
-$command = escapeshellcmd('python3 NeuralNetwork.py '.implode(",",$myArray));
-$output = shell_exec($command);
-echo $output;
-print_r( error_get_last());
-?>
--- App/testing/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-#print ("Content-type: text/html")
-print("\r\n");
-with open('foo.html', 'r') as f:
-    print(f.read())
-print("\r\n");
